43cb750b948eb4415d8e47bc4edce3fed7798aad
tugcem yalcin <tugcemyalcin@tugcems-MacBook.local>
2014-08-24 19:47:56 +0300

    Add some comments.

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var app = express();
+
+//file stream
 var fs = require('fs');
+
+//make async all requests
 var async = require('async');
+
+//include newrelic
 require('newrelic');
+
+//use for big numbers for PI calculation
 var decimal = require('decimal.js', function(Decimal) {
 // Use Decimal here in local scope. No global Decimal.
 });
 
-
+// chudnovsky algorithm for calculating 22/7 which gets 1000th digits of PI
 function chudnovsky(digits) {
     decimal.precision = digits + 2;
 
@@ -42,12 +50,13 @@ function chudnovsky(digits) {
     // pi = ( sqrt(10005) * 426880 ) / sum
     return decimal.sqrt(10005).times(426880).div(sum).toSD(digits);
 }
-
+//first request handler for calculating PI and then repsonding
 app.get('/shoot-for-my-cpu', function(req, res) {
-
+    //makes waterfall all step actions for calculating
     async.waterfall([function(callback) {
         result = chudnovsky(1000);
         callback(null, result);
+        //for creating response
     }, function(arg1, callback) {
         if(arg1) {
             STATUS_CODE = 1
@@ -58,6 +67,7 @@ app.get('/shoot-for-my-cpu', function(req, res) {
         }
         json = {"status": [STATUS_CODE], "msg": [STATUS_MESSAGE]};
         callback(null, json);
+        //and send response
     }, function (arg1, callback) {
         res.send([arg1]);
     }], function(err, result){
@@ -65,7 +75,9 @@ app.get('/shoot-for-my-cpu', function(req, res) {
     });
 });
 
+//second request handler for creating data to write file and then writes it and give feedback
 app.get('/shoot-for-my-disk', function(req, res) {
+    //parallel execution for data need to write file
     async.parallel([
         function(callback) {
             buffer = '[' + req.ip +']::';
@@ -80,6 +92,7 @@ app.get('/shoot-for-my-disk', function(req, res) {
             buffer = '[' + RANDOM_STRING + ']';
             callback(null,buffer);
         }], function(err, results) {
+            //writes serially gathered data to file and give response
             async.series([
                 function(callback){
                    fs.writeFile('userLog.txt', JSON.stringify(results), function (err) {
@@ -98,24 +111,5 @@ app.get('/shoot-for-my-disk', function(req, res) {
     });
 });
 
- //    USER_IP_ADDRESS = req.ip;
- //    TIMESTAMP = Date.now();
-
- //    for(var RANDOM_STRING = ''; RANDOM_STRING.length < 255;) {
- //        RANDOM_STRING += Math.random().toString(36).substr(2, 1)
- //    }
-
-// 	fs.writeFile('userLog.txt', '[' + USER_IP_ADDRESS+ ']::[' + TIMESTAMP+ ']::['+ RANDOM_STRING + ']', function (err) {
-// 	  if(err) {
-//     	   STATUS_CODE = 0
-//     	   STATUS_MESSAGE = err.toString();
-// 	   } else {
-//     	   STATUS_CODE = 1
-//     	   STATUS_MESSAGE = "";
-// 		}
-// 		res.send([{"status": [STATUS_CODE],"msg": [STATUS_MESSAGE]}]);	
-// 	});
-// });
- 
 app.listen(3000);
 console.log('Listening on port 3000...');
